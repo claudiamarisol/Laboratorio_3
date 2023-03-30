7f459cb831adb301d5b32ced907afecd7400159b
claudiamarisol <128396290+claudiamarisol@users.noreply.github.com>
2023-03-29 22:48:39 -0400

    se envio laboratorio_3

---
+++ ejercicio3_6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php  echo"<pre>
+        CCCCCCCC        A        CCCCCCCC   H       H    I
+        C             A   A      C          H       H    I
+        C            A     A     C          H       H    I
+        C           A       A    C          HHHHHHHHH    I
+        C           AAAAAAAAA    C          H       H    I
+        C           A       A    C          H       H    I
+        CCCCCCCC    A       A    CCCCCCCC   H       H    I
+    </pre>"
+?>
+</body>
+</html>
